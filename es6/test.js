5b546d1968843fbb5ed45ee7ae1a54489010a455
DockerBB <30626509+DockerBB@users.noreply.github.com>
2019-07-12 19:27:58 +0800

    添加ESLint

--- es6/test.js
+++ es6/test.js
@@ -1,15 +1,15 @@
 'use strict'
-const sentences =[
-    {subject: 'JavaScript',verb:'is',object:'great'},
-    {subject: 'Elephants',verb:'are',object:'large'},
-];
+const sentences = [
+    { subject: 'JavaScript', verb: 'is', object: 'great' },
+    { subject: 'Elephants', verb: 'are', object: 'large' }
+]
 
-//es6特性：对象解构
-function say({subject,verb,object}) {
-    //es6特性：模板字符串
-    console.log(`${subject}  ${verb} ${object}`);
+// es6特性对象解构
+function say ({ subject, verb, object }) {
+    // es6特性：模板字符串
+    console.log(`${subject}  ${verb} ${object}`)
 }
-//es6特性：for..of
-for(let s of sentences){
-    say(s);
+    // es6特性：for..of
+for (const s of sentences) {
+    say(s)
 }
